52c1accb3da3ff72d14432bed46f95c89605575b
Omer Ben-Amram <omerbenamram@gmail.com>
2023-12-09 09:46:03 +0200

    comments

--- rust/src/bin/day9.rs
+++ rust/src/bin/day9.rs
@@ -9,8 +9,9 @@ fn calc_deltas_recursive(sequence: &[i64]) -> Vec<Vec<i64>> {
 
     while {
         let delta: Vec<i64> = current.windows(2).map(|w| w[1] - w[0]).collect();
-        deltas.push(delta.clone());
-        !delta.iter().all(|&d| d == 0) // stop when all delta is zeros.
+        let stop = delta.iter().all(|&d| d == 0); // stop when all delta is zeros.
+        deltas.push(delta); // calculate stop first to avoid clone :)
+        !stop
     } {
         current = deltas.last().unwrap();
     }
