9a829fa03608f8f9c7da22c8ef851f02d21f016e
Omer Ben-Amram <omerbenamram@gmail.com>
2023-12-16 11:23:13 +0200

    lib code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,3 +1,5 @@
+use grid::Grid;
+
 #[macro_export]
 macro_rules! regex {
     ($re:expr $(,)?) => {{
@@ -5,4 +7,45 @@ macro_rules! regex {
             once_cell::sync::Lazy::new(|| regex::Regex::new($re).unwrap());
         &LAZY
     }};
+}
+
+pub trait CoordExt {
+    fn left(&self) -> (i64, i64);
+    fn right(&self) -> (i64, i64);
+    fn up(&self) -> (i64, i64);
+    fn down(&self) -> (i64, i64);
+}
+
+impl CoordExt for (i64, i64) {
+    fn left(&self) -> (i64, i64) {
+        (self.0, self.1 - 1)
+    }
+
+    fn right(&self) -> (i64, i64) {
+        (self.0, self.1 + 1)
+    }
+
+    fn up(&self) -> (i64, i64) {
+        (self.0 - 1, self.1)
+    }
+
+    fn down(&self) -> (i64, i64) {
+        (self.0 + 1, self.1)
+    }
+}
+
+pub trait GridExt<T> {
+    // Grid expects usize, and we always need to manually check for bounds which is annoying.
+    fn get_coordinate(&self, coord: (i64, i64)) -> Option<&T>;
+}
+
+impl<T> GridExt<T> for Grid<T> {
+    fn get_coordinate(&self, coord: (i64, i64)) -> Option<&T> {
+        // if we didn't fall off map (usize)
+        if (coord.0 < 0) || (coord.1 < 0) {
+            None
+        } else {
+            self.get(coord.0 as usize, coord.1 as usize)
+        }
+    }
 }
